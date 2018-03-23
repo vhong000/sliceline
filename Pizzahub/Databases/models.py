e974a1dbb099eb615f04283f1dd4b2c0bae44f46
Carlos <carlosngsang.cs@gmail.com>
2018-03-23 16:50:26 -0400

    finished the database

--- Pizzahub/Databases/models.py
+++ Pizzahub/Databases/models.py
@@ -1,26 +1,15 @@
 from django.db import models
 
+class restaurant(models.Model):
+    rest_id    = models.AutoField(primary_key=True)
+    name       = models.CharField(max_length=40)
+    address    = models.CharField(max_length=30)
+    city       = models.CharField(max_length=10)
+    state      = models.CharField(max_length=5)
+    zipcode    = models.CharField(max_length=5)
+    phone      = models.CharField(max_length=10)
 
-# Create your models here.
-#models are database tables, and each variable are the columns
-# class Album(models.Model):
-#     artist = models.CharField(max_length=25)
-#     album_title = models.CharField(max_length=50)
-#     genre = models.CharField(max_length=10)
-#     album_logo = models.CharField(max_length=1000) #this will link a url containing the logo
-#
-#     def get_absolute_url(self):
-#         return reverse('music:details',kwargs={'pk':self.pk})
-#
-# class Song(models.Model):
-#     album = models.ForeignKey(Album, on_delete=models.CASCADE)
-#     file_type = models.CharField(max_length=10)
-#     song_title = models.CharField(max_length=40)
-#     is_favorite = models.BooleanField(default=False)
-#
-#     def __str__(self):
-#         return self.song_title
-#Table for the employees
+
 class employees(models.Model):
     emp_id     = models.AutoField(primary_key=True)
     password   = models.CharField(max_length=25)
@@ -39,30 +28,94 @@ class employees(models.Model):
     complaints = models.IntegerField()
     last_order = models.DateTimeField()
 
-
-class users(models.Model):
-    user_id = models.CharField(primary_key=True)
-    password = models.CharField(max_length=25)
-    user_fname = models.CharField(max_length=25)
-    user_lname = models.CharField(max_length=25)
-    address = models.CharField(max_length=30)
-    city = models.CharField(max_length=10)
-    state = models.CharField(max_length=5)
-    zipcode = models.CharField(max_length=5)
-    phone = models.CharField(max_length=10)
-    birthday = models.DateField()
-    memb_since = models.DateTimeField()
-    wallet = models.IntegerField()
-    VIP = models.BooleanField()
-    warning    = models.IntegerField()
+#Table for the customer
+class customer(models.Model):
+    user_id     = models.CharField(primary_key=True)
+    password    = models.CharField(max_length=25)
+    user_fname  = models.CharField(max_length=25)
+    user_lname  = models.CharField(max_length=25)
+    address     = models.CharField(max_length=30)
+    city        = models.CharField(max_length=10)
+    state       = models.CharField(max_length=5)
+    zipcode     = models.CharField(max_length=5)
+    phone       = models.CharField(max_length=10)
+    birthday    = models.DateField()
+    memb_since  = models.DateTimeField()
+    wallet      = models.DecimalField(decimal_places=5,max_digits=2)
+    VIP         = models.BooleanField()
+    warning     = models.IntegerField()
     order_count = models.IntegerField()
-    complaints = models.IntegerField()
-    last_order = models.DateTimeField()
+    complaints  = models.IntegerField()
+    last_order  = models.DateTimeField()
 
-class rating(models.Model):
-    user_id = models.ForeignKey('users',on_delete=models.CASCADE)
-    menu_id= models.CharField(max_length=10)
-    pizza_item= models.CharField(max_length=10)
-    rating = models.IntegerField()
+#Table for chef
+class chef(models.Model):
+    chef_id   = models.AutoField(primary_key=True)
+    emp_id    = models.ForeignKey(employees,on_delete=models.CASCADE)
+    menu_name = models.CharField(max_length=10)
+    warning   = models.IntegerField()
+
+#Table for delivery guy
+class delivery(models.Model):
+    deliver_id = models.AutoField(primary_key=True)
+    emp_id     = models.ForeignKey(employees,on_delete=models.CASCADE)
+    status     = models.BooleanField()
+    warning    = models.IntegerField()
+
+#Table for menu
+class menu(models.Model):
+    menu_id     = models.AutoField(primary_key=True)
+    chef_id     = models.ForeignKey(chef,on_delete=models.CASCADE)
+    price       = models.DecimalField(decimal_places=5,max_digits=2)
+    description = models.CharField(max_length=100)
+    rating      = models.IntegerField()
+    picture     = models.CharField(max_length=1000)
+
+#Table for ordering
+class order(models.Model):
+    order_id     = models.AutoField(primary_key=True)
+    rest_id      = models.ForeignKey(restaurant,on_delete=models.CASCADE)
+    menu_id      = models.ForeignKey(menu,on_delete=models.CASCADE)
+    total        = models.IntegerField()
+
+#Table for the customer_review
+class customer_review(models.Model):
+    review_id       = models.AutoField(primary_key=True)
+    user_id         = models.ForeignKey(customer,on_delete=models.CASCADE)
+    order_number    = models.ForeignKey(order,on_delete=models.CASCADE)
+    pizza_rating    = models.IntegerField()
+    store_rating    = models.IntegerField()
+    delivery_rating = models.IntegerField()
+
+#Table for the delivery_review
+class delivery_review(models.Model):
+    review_id       = models.AutoField(primary_key=True)
+    emp_id          = models.ForeignKey(employees,on_delete=models.CASCADE)
+    customer_rating = models.CharField(max_length=100)
+
+#Table for checkout
+class checkout(models.Model):
+    user_id = models.ForeignKey(customer, on_delete=models.CASCADE)
+    menu_id = models.ForeignKey(menu, on_delete=models.CASCADE)
+    total   = models.DecimalField(decimal_places=5,max_digits=2)
+
+#Table for complaints
+class complaint(models.Model):
+    complaint_id    = models.AutoField(primary_key=True)
+    user_id         = models.ForeignKey(customer,on_delete=models.CASCADE)
+    emp_id          = models.ForeignKey(employees,on_delete=models.CASCADE)
+    complaint       = models.CharField(max_length=1000)
+    approval        = models.BooleanField()
+
+#Table for compliment
+class compliment(models.Model):
+    compliment_id   = models.AutoField(primary_key=True)
+    user_id         = models.ForeignKey(customer,on_delete=models.CASCADE)
+    emp_id          = models.ForeignKey(employees,on_delete=models.CASCADE)
+    compliment      = models.CharField(max_length=1000)
+    approval        = models.BooleanField()
+
+
+
 
 
