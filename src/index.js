a199b49791ed5dcc9e52f91d61f70c02c863f1c1
victor <victorhong04@gmail.com>
2018-04-03 00:12:11 -0400

    more files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Login from './components/login.js';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route exact path="/" component={App}/>
+      <Route exact path="/login" component={Login}/>
+    </div>
+  </Router>,
+  document.getElementById('root'));
 registerServiceWorker();
