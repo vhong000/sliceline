ab6723ced259767074dd7b8678f348a58e52921f
victor <victorhong04@gmail.com>
2018-04-03 22:23:39 -0400

    added city and fixed up some things

--- Sliceline/settings.py
+++ Sliceline/settings.py
@@ -91,10 +91,10 @@ WSGI_APPLICATION = 'Sliceline.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': '/home/victor/csc322/sliceline/db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
