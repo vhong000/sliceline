5dbfd2e13e55cf96e144a6e83ab7157510d5d28e
victor <victorhong04@gmail.com>
2018-04-03 22:52:42 -0400

    renamed Pizzahub to Slicehub in some files

--- Sliceline/settings.py
+++ Sliceline/settings.py
@@ -91,8 +91,7 @@ WSGI_APPLICATION = 'Sliceline.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': '/home/victor/csc322/sliceline/db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
--- Sliceline/wsgi.py
+++ Sliceline/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Pizzahub project.
+WSGI config for Sliceline project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Pizzahub.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Sliceline.settings")
 
 application = get_wsgi_application()
