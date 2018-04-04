f8d2b47dfa7c797c47bbd3eaf4a2ed24fe21c07a
Carlos <carlosngsang.cs@gmail.com>
2018-04-04 12:48:13 -0400

    added chef's functions

--- tables/functions.py
+++ tables/functions.py
@@ -9,18 +9,22 @@ conn = sqlite3.connect("/home/cng000/Documents/CSC322/sliceline/db.sqlite3")
 cursor = conn.cursor()
 from django.utils import timezone
 
+#Employees sign up function to insert into the DB
 def employeeSignUp(username,lastname,password,address,city,state,zipcode,phone,ssn,birthday,email):
+    date = timezone.now()
     hash_object = hashlib.sha256(password.encode('utf-8'))
     hex_dig = hash_object.hexdigest()
     cursor.execute("""insert into tables_employees 
-                  (password, emp_fname, emp_lname, address, city, state, zipcode, phone, ssn, birthday,email)"""
-                  """VALUES (?,?,?,?,?,?,?,?,?,?,?)""",
-                   [hex_dig,username,lastname,address,city,state,zipcode,phone,ssn,birthday,email])
+                  (password, emp_fname, emp_lname, address, city, state, zipcode, phone, ssn, birthday,email,date_hired)"""
+                  """VALUES (?,?,?,?,?,?,?,?,?,?,?,?)""",
+                   [hex_dig,username,lastname,address,city,state,zipcode,phone,ssn,birthday,email,date])
+    #need a wait function for manager approval before inserting to DB
     conn.commit()
-    cursor.execute("""INSERT INTO tables_account (type, email)"""
+    cursor.execute("""INSERT INTO tables_account (types, email)"""
                    """VALUES (?,?)""", ["employees", email])
+    conn.commit()
 
-
+#Customer sign up function to insert into the DB
 def customerSignUp(username,lastname,password,address,city,state,zipcode,phone,birthday,email):
     date = timezone.now()
     hash_object = hashlib.sha256(password.encode('utf-8'))
@@ -29,14 +33,15 @@ def customerSignUp(username,lastname,password,address,city,state,zipcode,phone,b
                   (password, user_fname, user_lname, address, city, state, zipcode, phone, birthday,email,memb_since)"""
                   """VALUES (?,?,?,?,?,?,?,?,?,?,?)""",
                    [hex_dig,username,lastname,address,city,state,zipcode,phone,birthday,email,date])
+    #need a wait function for manager approval before inserting to DB
     conn.commit()
-    cursor.execute("""INSERT INTO tables_account (type, email)"""
+    cursor.execute("""INSERT INTO tables_account (types, email)"""
                    """VALUES (?,?)""",["customer",email])
     conn.commit()
 
-
+#Checks if the email belongs to a customer or employee
 def checkEmail(email):
-    cursor.execute("""SELECT type FROM tables_account WHERE email=?""",[email])
+    cursor.execute("""SELECT types FROM tables_account WHERE email=?""",[email])
     row = cursor.fetchone()
     type = row[0]
     if (type == "customer"):
@@ -44,6 +49,7 @@ def checkEmail(email):
     else:
         return False
 
+#Authenticate the login
 def login(email,password):
     hash_object = hashlib.sha256(password.encode('utf-8'))
     hex_dig = hash_object.hexdigest()
@@ -51,17 +57,39 @@ def login(email,password):
         cursor.execute("""select password from tables_customer WHERE email=?""",[email])
         row = cursor.fetchone()
         hash_password = row[0]
+        cursor.execute("""select user_id from main.tables_customer WHERE email=?""",[email])
+        row = cursor.fetchone()
+        user_id = row[0]
+        return  user_id
     else:
-        cursor.execute("""select password from tables_employees WHERE email=?""", [email])
+        cursor.execute("""select password from tables_employees WHERE email=?""",[email])
         row = cursor.fetchone()
         hash_password = row[0]
+        cursor.execute("""select emp_id from main.tables_employees WHERE email=?""",[email])
+        row = cursor.fetchone()
+        emp_id  = row[0]
+        return emp_id
 
     if(hex_dig == hash_password):
         return True
+#chef creates a menu
+def createMenu(chef_id,price,description,picture):
+    cursor.execute("""INSERT INTO main.tables_menu (price, description, rating, picture, chef_id_id)""" 
+                    """VALUES (?,?,?,?,?)""",[price,description,0,picture,chef_id])
+    conn.commit()
+#chef changes the price on a menu
+def updatePrices(price,menu_id):
+    cursor.execute("""UPDATE main.tables_menu set price = ? WHERE main.tables_menu.menu_id= ?""",
+                   [price,menu_id])
+    conn.commit()
 
+#Will need a how menu function
+#Function to be done: Place order, Review, Delivery, Manager
 
 print("ran")
-# customerSignUp('andrii','luchko','123','CCNY','harlem','NY','11013','123-231-0123','11/12/1996',"test@gmail.com")
+# employeeSignUp('andrii','luchko','123','CCNY','harlem','NY','11013','123-231-0123','23456875','11/12/1996',"test@gmail.com")
 # print(login('test@gmail.com','123'))
+# createMenu('1',12,"cheese pie","no picture")
+# updatePrices("12.99",'1')
 print("done")
 conn.close()
--- tables/models.py
+++ tables/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 
 class restaurant(models.Model):
+    rest_id    = models.AutoField(primary_key=True)
     name       = models.CharField(max_length=40)
     address    = models.CharField(max_length=30)
     city       = models.CharField(max_length=10)
@@ -11,6 +12,7 @@ class restaurant(models.Model):
 
 
 class employees(models.Model):
+    emp_id     = models.AutoField(primary_key=True)
     password   = models.CharField(max_length=25)
     emp_fname  = models.CharField(max_length=25)
     emp_lname  = models.CharField(max_length=25)
@@ -21,15 +23,16 @@ class employees(models.Model):
     phone      = models.CharField(max_length=10)
     ssn        = models.CharField(max_length=9)
     birthday   = models.DateField()
-    salary     = models.DecimalField(decimal_places=2,max_digits=5)
+    salary     = models.DecimalField(decimal_places=2,max_digits=5,null=True)
     date_hired = models.DateTimeField(null=True)
     num_compliment = models.IntegerField(null=True)
     num_complaint = models.IntegerField(null=True)
     last_order = models.DateTimeField(null=True)
-    email = models.CharField(max_length=100,default='DEFAULT VALUE')
+    email = models.CharField(max_length=100)
 
 #Table for the customer
 class customer(models.Model):
+    user_id     = models.AutoField(primary_key=True)
     password    = models.CharField(max_length=25)
     user_fname  = models.CharField(max_length=25)
     user_lname  = models.CharField(max_length=25)
@@ -51,6 +54,7 @@ class customer(models.Model):
 
 #Table for chef
 class chef(models.Model):
+    chef_id    = models.AutoField(primary_key=True)
     emp_id    = models.ForeignKey(employees,on_delete=models.CASCADE)
     store      = models.ForeignKey(restaurant,on_delete=models.CASCADE)
     menu_name = models.CharField(max_length=10)
@@ -58,6 +62,7 @@ class chef(models.Model):
 
 #Table for delivery guy
 class delivery(models.Model):
+    deli_id    = models.AutoField(primary_key=True)
     emp_id     = models.ForeignKey(employees,on_delete=models.CASCADE)
     store      = models.ForeignKey(restaurant,on_delete=models.CASCADE)
     status     = models.BooleanField()
@@ -65,20 +70,23 @@ class delivery(models.Model):
 
 #Table for menu
 class menu(models.Model):
+    menu_id     = models.AutoField(primary_key=True)
     chef_id     = models.ForeignKey(chef,on_delete=models.CASCADE)
     price       = models.DecimalField(decimal_places=2,max_digits=5)
     description = models.CharField(max_length=100)
-    rating      = models.IntegerField()
-    picture     = models.CharField(max_length=1000)
+    rating      = models.IntegerField(null=True)
+    picture     = models.CharField(max_length=1000,null=True)
 
 #Table for ordering
 class order(models.Model):
+    order_id     = models.AutoField(primary_key=True)
     rest_id      = models.ForeignKey(restaurant,on_delete=models.CASCADE)
     menu_id      = models.ForeignKey(menu,on_delete=models.CASCADE)
     total        = models.IntegerField()
 
 #Table for the customer_review
 class customer_review(models.Model):
+    review_id       = models.AutoField(primary_key=True)
     user_id         = models.ForeignKey(customer,on_delete=models.CASCADE)
     emp_id          = models.ForeignKey(employees,on_delete=models.CASCADE)
     order_number    = models.ForeignKey(order,on_delete=models.CASCADE)
@@ -88,18 +96,21 @@ class customer_review(models.Model):
 
 #Table for the delivery_review
 class delivery_review(models.Model):
+    review_id       = models.AutoField(primary_key=True)
     emp_id          = models.ForeignKey(employees,on_delete=models.CASCADE)
     user_id         = models.ForeignKey(customer,on_delete=models.CASCADE)
     customer_rating = models.CharField(max_length=100)
 
 #Table for checkout
 class checkout(models.Model):
+    checkout_id       = models.AutoField(primary_key=True)
     user_id = models.ForeignKey(customer, on_delete=models.CASCADE)
     item    = models.CharField(max_length=50)
     total   = models.DecimalField(decimal_places=2,max_digits=5)
 
 #Table for complaints
 class complaints(models.Model):
+    complaint_id    = models.AutoField(primary_key=True)
     user_id         = models.ForeignKey(customer,on_delete=models.CASCADE)
     emp_id          = models.ForeignKey(employees,on_delete=models.CASCADE)
     complaint_text       = models.CharField(max_length=1000)
@@ -107,6 +118,7 @@ class complaints(models.Model):
 
 #Table for compliment
 class compliments(models.Model):
+    compliments_id  = models.AutoField(primary_key=True)
     user_id         = models.ForeignKey(customer,on_delete=models.CASCADE)
     emp_id          = models.ForeignKey(employees,on_delete=models.CASCADE)
     compliment_text      = models.CharField(max_length=1000)
@@ -114,7 +126,9 @@ class compliments(models.Model):
 
 
 class Account(models.Model):
-    type  = models.CharField(max_length=10)
-    email = models.CharField(max_length=100)
+    acc_id = models.AutoField(primary_key=True)
+    types  = models.CharField(max_length=10)
+    email  = models.CharField(max_length=100)
+
 
 
