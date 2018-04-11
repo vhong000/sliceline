e44fb143379218ea5ab579f37b285c1b038f576e
Carlos <carlosngsang.cs@gmail.com>
2018-04-11 15:41:11 -0400

    added api

--- tables/urls.py
+++ tables/urls.py
@@ -5,6 +5,25 @@ from .views import *
 router = DefaultRouter()
 
 router.register(r'restaurants', RestaurantViewSet, base_name='restaurants')
+router.register(r'customer', CustomerViewSet, base_name='customer')
+router.register(r'employees', EmployeesViewSet, base_name='employees')
+router.register(r'access', AccessViewSet, base_name='access')
+router.register(r'account', AccountViewSet, base_name='account')
+router.register(r'checkout', CheckoutViewSet, base_name='checkout')
+router.register(r'chef', ChefViewSet, base_name='chef')
+router.register(r'complaints', ComplaintsViewSet, base_name='complaints')
+router.register(r'compliments', ComplimentsViewSet, base_name='compliments')
+router.register(r'delivery', DeliveryViewSet, base_name='delivery')
+router.register(r'delivery_review', Delivery_reviewViewSet, base_name='delivery_review')
+router.register(r'menu', MenuViewSet, base_name='menu')
+router.register(r'order', OrderViewSet, base_name='order')
+
+
+
+
+
+
+
 
 urlpatterns = [
     url(r'^', include(router.urls)),
--- tables/views.py
+++ tables/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from tables.serializers import RestaurantSerializer
+from tables.serializers import *
 from .models import *
 
 class RestaurantViewSet(viewsets.ModelViewSet):
@@ -8,4 +8,54 @@ class RestaurantViewSet(viewsets.ModelViewSet):
     queryset = Restaurant.objects.all().order_by('rest_id')
     serializer_class = RestaurantSerializer
 
-# Create your views here.
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all().order_by('user_id')
+    serializer_class = CustomerSerializer
+
+class EmployeesViewSet(viewsets.ModelViewSet):
+    queryset = Employees.objects.all().order_by('emp_id')
+    serializer_class = EmployeesSerializer
+
+class AccessViewSet(viewsets.ModelViewSet):
+    queryset = Access.objects.all().order_by('access_id')
+    serializer_class = AccessSerializer
+
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset = Access.objects.all().order_by('acc_id')
+    serializer_class = AccessSerializer
+
+class CheckoutViewSet(viewsets.ModelViewSet):
+    queryset = Checkout.objects.all().order_by('user_id_id')
+    serializer_class = CheckoutSerializer
+
+class ChefViewSet(viewsets.ModelViewSet):
+    queryset = Chef.objects.all().order_by('chef_id')
+    serializer_class = ChefSerializer
+
+class ComplaintsViewSet(viewsets.ModelViewSet):
+    queryset = Complaints.objects.all().order_by('complaint_id')
+    serializer_class = CheckoutSerializer
+
+class ComplimentsViewSet(viewsets.ModelViewSet):
+    queryset = Compliments.objects.all().order_by('compliments_id')
+    serializer_class = ComplimentsSerializer
+
+class Customer_reviewViewSet(viewsets.ModelViewSet):
+    queryset = Customer_review.objects.all().order_by('review_id')
+    serializer_class = Customer_reviewSerializer
+
+class DeliveryViewSet(viewsets.ModelViewSet):
+    queryset = Delivery.objects.all().order_by('deli_id')
+    serializer_class = DeliverySerializer
+
+class Delivery_reviewViewSet(viewsets.ModelViewSet):
+    queryset = Delivery_review.objects.all().order_by('review_id')
+    serializer_class = Delivery_reviewSerializer
+
+class MenuViewSet(viewsets.ModelViewSet):
+    queryset = Menu.objects.all().order_by('menu_id')
+    serializer_class = MenuSerializer
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all().order_by('order_id')
+    serializer_class = OrderSerializer
