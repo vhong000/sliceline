eda4740d595e35a3aa9f4c730f25b64aa461ad44
Carlos <carlosngsang.cs@gmail.com>
2018-05-08 12:56:12 -0400

    added api/ingredient/

--- tables/urls.py
+++ tables/urls.py
@@ -23,13 +23,14 @@ router.register(r'custSignup',CSignupViewSet,base_name='custSignup')
 router.register(r'menu/update',UpdateMenuViewSet,base_name='menu_update')
 router.register(r'menu', MenuViewSet, base_name='menu')
 router.register(r'manager/approval',ApprovalViewSet,base_name='approval')
+router.register(r'ingredient',IngredientViewSet,base_name='create_pizza')
 
 
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^menu/chef/(?P<chef>\d+)/$',ListMenu), #this gets all the predefined menu from given chef
     url(r'^restaurants/chef/(?P<store>\d+)/$', Store_chef), #this gets all the chefs from given store
-    url(r'^restaurants/ingredient$', Ingredients), #gets all of the ingredients for build pizza
+    # url(r'^restaurants/ingredient$', Ingredients), #gets all of the ingredients for build pizza
     url(r'^manager/list$',listOfUnapproveCustomer), #gets all of the unapprove customer
 
 ]
